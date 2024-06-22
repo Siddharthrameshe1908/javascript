a29751636b820280ddb96434964306c4213489cb
Siddharthrameshe1908 <siddharth19082002@gmail.com>
2024-06-22 13:15:18 +0530

    upload

---
+++ Array.js
@@ -0,0 +1,78 @@
+// var x = [21,'js',true,undefined,60.00]
+// console.log(x);
+// console.log(x[1]);
+
+// var food = new Array
+// console.log(food);
+// food[0] = 'rice'
+// food[1] = 'chapathi'
+// // food[1] = 'briyani'
+// food[2] = 'Briyani'
+// food[3] = 'Panner cury'
+// food[4] = 'Curd cury'
+// console.log(food)
+
+// var nonVeg = new Array("Chicken biriyani","muuton curry","fish cury","tawa fish")
+// console.log(nonVeg)
+// console.log(nonVeg[2])
+
+// var z=[70,33,21,14,9]
+// console.log(z.sort())
+// var z1 = ['js','angular','ami','jreact']
+// console.log(z1.sort())
+
+// let x7 = [60,70,80,90,100]
+// console.log(x7.slice(2,4))
+// console.log(x7.slice(1,5))
+// console.log(x7)
+
+// var x8 = [60,70,80,90,100]
+// // console.log(x8.splice(2,1))
+// console.log(x8.splice(2,1,1,2,3,4))
+// console.log(x8)
+
+// var a = [19,11,108,100,103] disadvantage of sort method
+// console.log(a.sort())
+// console.log(a.pop())
+
+// var a = [10,20,39,38,338,23] disadvantage of sort method
+// console.log(a.sort())
+// console.log(a.shift())
+// console.log(a)
+
+var a = [11,10,28,38,18]
+// console.log(a.sort())
+// console.log(a.slice(4,5))
+// console.log(a) 
+// var a = "5"+1
+// console.log(a)
+// let result = "4" - 1;
+// console.log(result);  // Outputs: "string1"
+// console.log(Number.isInteger(result));
+
+// var r = "41.2s341"
+// console.log(Number.parseFloat(r));
+
+// var s = "siddharth"
+// console.log(s.charAt(Math.floor(Math.random()*8))) 
+// console.log(Math.floor(Math.random()*9))
+
+// var s = prompt("Opening")
+// console.log(s)
+
+var a = [1,2,3,4,5,6,2,5]
+// console.log(a.length);
+var count = 0
+// for(i=0;i<(a.length);i++){
+//     if(a.includes(a[i])){
+//         count = count + 1
+//     }
+// }
+// console.log(count);
+// var c = []
+// var res = a.filter((element)=>{
+//     if(!(c.includes(element))){
+//         c.push(element)
+//     }
+// })
+// console.log(c);
