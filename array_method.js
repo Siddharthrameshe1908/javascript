898116b60e143efae14e1875ecb5c129c8dedcb7
Siddharthrameshe1908 <siddharth19082002@gmail.com>
2024-06-22 13:14:02 +0530

    upload

---
+++ array_method.js
@@ -0,0 +1,44 @@
+// let arr = [31,21,56,78,93,7]
+// var res = arr.filter((ele,index,arr)=>{
+//     console.log(ele,index,arr);
+//     return ele>21;
+// })
+// console.log(arr)
+// console.log(res)
+
+// let arr = [31,21,56,78,93,7]
+// var r = arr.map((ele,index,arr)=>{
+//     return ele*2
+// })
+// console.log(r)
+// console.log(arr)  
+
+// let arr = [31,21,56,78,93,7]
+// var r = arr.reduce((pre,ele,index,arr)=>{
+//     console.log(pre,ele,index,arr);
+//     return (ele+pre)<7;
+// },0)
+// console.log(r)
+
+// let emp = {
+//     Ename: 'mala',
+//     Eid: 21,
+//     Esal: 40000
+// }
+// for(let x in emp){
+//     console.log(`${x}:${emp[x]}`);
+// }
+
+// let emp={
+//     Ename: 'mala',
+//     Eid: 21,
+//     Esal: 40000
+// }
+// for(let x of Object.keys(emp)){
+//     console.log(`${x}:${emp[x]}`);
+// }
+
+// let x = [1,2,3,3,4,5,6]
+// x.forEach((ele)=>{
+//     console.log(ele);
+// })
